b3ac02fea39869de44f3630e1b383b7045f1da46
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-02-12 21:38:36 +0530

    Create registerview

--- authsystem/users/views.py
+++ authsystem/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
+from .serializers import UserSerializer, RegisterSerializer
+from django.contrib.auth import get_user_model
 
 # Create your views here.
+
+User=get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset=User.objects.all()
+    permission_classes=(AllowAny,)
+    serializer_class= RegisterSerializer
+
+
