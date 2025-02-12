ce5a47fc93433a66b26814902a45075d174a91e8
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-02-12 21:40:11 +0530

    Update serializers.py

--- authsystem/users/serializers.py
+++ authsystem/users/serializers.py
@@ -33,4 +33,4 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
-        return super().create(validated_data)
+        
