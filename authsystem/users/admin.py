b42135b2bd480c8a2a85b8431cbe1a2d56111f10
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-02-12 22:08:06 +0530

    Update admin.py

--- authsystem/users/admin.py
+++ authsystem/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+class UsersAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'is_active', 'created_at')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+
+admin.site.register(User, UsersAdmin)
